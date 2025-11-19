ebcf36d392690fcfd91d1a7a7026b74dc5b927c0
Pramod Pandit <nwdpramod12305901@gmail.com>
2025-11-19 14:47:52 +0530

    Add solution for Keep Multiplying Found Values by Two

--- LeetCode/Keep-Multiplying-Found-Values-by-Two/Solution.cpp
+++ LeetCode/Keep-Multiplying-Found-Values-by-Two/Solution.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int findFinalValue(vector<int>& nums, int original) {
-        unordered_set<int> s(nums.begin(), nums.end());
+        sort(nums.begin(), nums.end());
 
-        while (s.count(original)) {
-            original *= 2;
+        for (int x : nums) {
+            if (x == original) {
+                original *= 2;
+            }
         }
 
         return original;
