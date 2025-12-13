d620b20dae2fbac2e7481efbfbe4e0b19d06a3e7
Pramod Pandit <nwdpramod12305901@gmail.com>
2025-12-13 20:40:06 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.cpp
+++ LeetCode/Valid-Anagram/Solution.cpp
@@ -1,17 +1,30 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        if (s.size() != t.size())
-            return false;
-        vector<int> h(26, 0);
-        for (int i = 0; i < s.size(); i++) {
-            h[s[i] - 'a']++;
-        }
-        for (int i = 0; i < t.size(); i++) {
-            int x = --h[t[i] - 'a'];
-            if (x < 0)
-                return false;
-        }
-        return true;
-    }
-};
+1// class Solution {
+2// public:
+3//     bool isAnagram(string s, string t) {
+4//         if (s.size() != t.size())
+5//             return false;
+6//         vector<int> h(26, 0);
+7//         for (int i = 0; i < s.size(); i++) {
+8//             h[s[i] - 'a']++;
+9//         }
+10//         for (int i = 0; i < t.size(); i++) {
+11//             int x = --h[t[i] - 'a'];
+12//             if (x < 0)
+13//                 return false;
+14//         }
+15//         return true;
+16//     }
+17// };
+18
+19class Solution {
+20public:
+21    bool isAnagram(string s, string t) {
+22        if (s.length() != t.length())
+23            return false;
+24
+25        sort(s.begin(), s.end());
+26        sort(t.begin(), t.end());
+27
+28        return s == t;
+29    }
+30};
