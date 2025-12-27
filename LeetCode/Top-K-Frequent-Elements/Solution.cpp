098a436944b9b2b5fe9bb31ae303178fce205824
Pramod Pandit <nwdpramod12305901@gmail.com>
2025-12-27 09:49:03 +0530

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.cpp
+++ LeetCode/Top-K-Frequent-Elements/Solution.cpp
@@ -1,26 +1,6 @@
-1class Solution {
-2public:
-3    typedef pair<int, int> p;
-4    vector<int> topKFrequent(vector<int>& nums, int k) {
-5
-6        priority_queue<p, vector<p>, greater<p>> pq;
-7
-8        unordered_map<int, int> mp;
-9        for(int i : nums)
-10            mp[i]++;
-11
-12        for(auto it:mp) {
-13            pq.push({it.second, it.first});
-14
-15            if(pq.size() > k)
-16                pq.pop();
-17        }
-18
-19        vector<int> result;
-20        while(!pq.empty()){
-21            result.push_back(pq.top().second);
-22            pq.pop();
-23        }
-24        return result;
-25    }
-26};
+def topKFrequent(self, nums, k):
+    bucket = [[] for _ in range(len(nums) + 1)]
+    Count = Counter(nums).items()  
+    for num, freq in Count: bucket[freq].append(num) 
+    flat_list = [item for sublist in bucket for item in sublist]
+    return flat_list[::-1][:k]
