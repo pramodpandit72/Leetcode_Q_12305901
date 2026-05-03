10f7b6dc5cd0b207c701dda8600f58b61e69175a
Pramod Pandit <nwdpramod12305901@gmail.com>
2026-05-03 13:37:56 +0530

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.cpp
+++ LeetCode/Two-Sum/Solution.cpp
@@ -1,12 +1,15 @@
- for(var i =0;i<nums.length;i++){
-    var item = nums[i];   
-    for(var j=0; j < nums.length;j++){
-        var item2 = nums[j];
-        var sum = item + item2;
-        
-    if(sum === target ){
-      return [i,j];
-      
-    }
-  }
- }
+1class Solution {
+2public:
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        int n = nums.size();
+5
+6        for(int i = 0; i < n; i++) {
+7            for(int j = i + 1; j < n; j++) {
+8                if(nums[i] + nums[j] == target) {
+9                    return {i, j};
+10                }
+11            }
+12        }
+13        return {-1, -1};
+14    }
+15};
