c44adbc62573b9b5e5c62af77058624fe2c86d50
Pramod Pandit <nwdpramod12305901@gmail.com>
2026-06-26 10:22:59 +0530

    Add solution for Maximum Subarray

--- LeetCode/Maximum-Subarray/Solution.cpp
+++ LeetCode/Maximum-Subarray/Solution.cpp
@@ -1,12 +1,12 @@
 1class Solution {
 2public:
 3    int maxSubArray(vector<int>& nums) {
-4        int currSum = nums[0];
+4        int currentSum = nums[0];
 5        int maxSum = nums[0];
 6
-7        for(int i = 1; i < nums.size(); i++){
-8            currSum = max(nums[i], currSum + nums[i]);
-9            maxSum = max(maxSum, currSum);
+7        for(int i=1; i<nums.size(); i++) {
+8            currentSum = max(nums[i], currentSum + nums[i]);
+9            maxSum = max(maxSum, currentSum);
 10        }
 11        return maxSum;
 12    }
