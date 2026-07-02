4482870aafc7a3204a4200536b074c67421a0ed5
Pramod Pandit <nwdpramod12305901@gmail.com>
2026-07-02 10:58:09 +0530

    Add solution for Container With Most Water

--- LeetCode/Container-With-Most-Water/Solution.cpp
+++ LeetCode/Container-With-Most-Water/Solution.cpp
@@ -1,24 +1,36 @@
-class Solution {
-public:
-    int maxArea(vector<int>& height) {
-        int n = height.size();
-        int i = 0, j = n - 1;
-
-        int water = 0;
-        while (i < j) {
-
-            // start from the smallest one and calculate water
-            int h = min(height[i], height[j]);
-            int w = j - i;
-            int area = h * w;
-            water = max(water, area);
-
-            if (height[i] < height[j])
-                i++;
-            else
-                j--;
-        }
-
-        return water;
-    }
-};
+1class Solution {
+2public:
+3    int maxArea(vector<int>& height) {
+4        int maxArea = 0;
+5        int left = 0;
+6        int right = height.size() - 1;
+7
+8        while(left < right) {
+9            int w = right - left;
+10            int h = min(height[left], height[right]);
+11
+12            maxArea = max(maxArea, w * h);
+13
+14            if(height[left] < height[right]) {
+15                left++;
+16            } else {
+17                right--;
+18            }
+19        }
+20
+21        return maxArea;
+22
+23
+24        // int maxArea = 0;
+25
+26        // for(int i = 0; i < height.size(); i++) {
+27        //     for(int j = i + 1; j < height.size(); j++) {
+28        //         int w = j - i;
+29        //         int h = min(height[i], height[j]);
+30
+31        //         maxArea = max(maxArea, h * w);
+32        //     }
+33        // }
+34        // return maxArea;
+35    }
+36};
