0b811dabff81c9c127655dc30b9adc0da77937fb
Pramod Pandit <nwdpramod12305901@gmail.com>
2026-07-06 10:43:52 +0530

    Add solution for Top K Frequent Elements

--- LeetCode/Top-K-Frequent-Elements/Solution.cpp
+++ LeetCode/Top-K-Frequent-Elements/Solution.cpp
@@ -1,6 +1,41 @@
-def topKFrequent(self, nums, k):
-    bucket = [[] for _ in range(len(nums) + 1)]
-    Count = Counter(nums).items()  
-    for num, freq in Count: bucket[freq].append(num) 
-    flat_list = [item for sublist in bucket for item in sublist]
-    return flat_list[::-1][:k]
+1class Solution {
+2public:
+3    vector<int> topKFrequent(vector<int>& nums, int k) {
+4        int n = nums.size();
+5
+6        unordered_map<int, int> mp;
+7
+8        for (int& num : nums) {
+9            mp[num]++;
+10        }
+11
+12        // index = frequency
+13        // Value will be elements
+14        // bucket[i] = elements occuring ith time
+15        vector<vector<int>> bucket(n + 1);
+16
+17        for (auto& it : mp) {
+18            int element = it.first;
+19            int freq = it.second;
+20
+21            bucket[freq].push_back(element);
+22        }
+23
+24        // Pick from right to left to find max frequency elements
+25        vector<int> result;
+26        for (int i = n; i >= 0; i--) {
+27
+28            if (bucket[i].size() == 0)
+29                continue;
+30
+31            int size = bucket.size();
+32            while (bucket[i].size() > 0 && k > 0) {
+33                result.push_back(bucket[i].back());
+34                bucket[i].pop_back();
+35                k--;
+36            }
+37        }
+38
+39        return result;
+40    }
+41};
