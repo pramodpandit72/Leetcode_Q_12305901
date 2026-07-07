25ce49df6e6199c62049f627c4586961d51b2f04
Pramod Pandit <nwdpramod12305901@gmail.com>
2026-07-07 21:22:22 +0530

    Add solution for Group Anagrams

--- LeetCode/Group-Anagrams/Solution.cpp
+++ LeetCode/Group-Anagrams/Solution.cpp
@@ -3,16 +3,19 @@
 3    vector<vector<string>> groupAnagrams(vector<string>& strs) {
 4        unordered_map<string, vector<string>> mp;
 5
-6        for(auto str:strs) {
-7            string temp = str;
-8            sort(temp.begin(), temp.end());
-9            mp[temp].push_back(str);
-10        }
-11
-12        vector<vector<string>> result;
-13        for(auto it : mp){
-14            result.push_back(it.second);
-15        }
-16        return result;
-17    }
-18};
+6        for (string s : strs) {
+7            string key = s;
+8            sort(key.begin(), key.end());   
+9
+10            mp[key].push_back(s);
+11        }
+12
+13        vector<vector<string>> ans;
+14
+15        for (auto &it : mp) {
+16            ans.push_back(it.second);
+17        }
+18
+19        return ans;
+20    }
+21};
