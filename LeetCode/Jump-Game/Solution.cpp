b84ddb967066493fca91c6821ccd7864acf19e25
Pramod Pandit <nwdpramod12305901@gmail.com>
2026-07-13 10:08:33 +0530

    Add solution for Jump Game

--- LeetCode/Jump-Game/Solution.cpp
+++ LeetCode/Jump-Game/Solution.cpp
@@ -1,19 +1,18 @@
 1class Solution {
 2public:
 3    bool canJump(vector<int>& nums) {
-4        int n = nums.size();
-5        int maxReach = 0;
-6
-7        for(int i = 0; i < n; i++) {
-8            if(i > maxReach) {
-9                return false;
-10            }
-11            maxReach = max(i + nums[i], maxReach);
-12        }
-13        if(maxReach >= n - 1){
-14            return true;
-15        }
-16
-17        return false;
-18    }
-19};
+4        int maxReach = 0;
+5
+6        for(int i = 0; i < nums.size(); i++) {
+7            if(i > maxReach) {
+8                return false;
+9            }
+10            maxReach = max(i + nums[i], maxReach);
+11        }
+12        if(maxReach >= nums.size() - 1){
+13            return true;
+14        }
+15
+16        return false;
+17    }
+18};
